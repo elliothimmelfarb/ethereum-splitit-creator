0caf544a7920045f5f7ea0531b079a0150608849
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-01-27 13:01:21 -0700

    added materialize, switched buttons to new style, reformatted js code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log({Web3})
-
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
@@ -10,127 +8,95 @@ if (typeof web3 !== 'undefined') {
 var contractCode
 
 web3.eth.getAccounts((error, result) => {
-		
 	if (!error) {
 		web3.eth.defaultAccount = result[0]
 	}
-	
-	console.log(error || result)	
-		
+	console.log(error || result)
 })
 
 function createContractCode() {
-	
 	var addressesString = document.getElementById("addresses").value
-	
-	var addresses = addressesString.split(",").map(a => a.trim()) 
-	
+	var addresses = addressesString.split(",").map(a => a.trim())
+	contractCode = `
+    pragma solidity ^0.4.4;
 
-	
-	contractCode = `pragma solidity ^0.4.4;
+    contract SplitIt {
 
-contract SplitIt {
-    
-    address[] employees = [${addresses.join(", ") }];
-    uint totalReceived;
-    mapping (address => uint) withdrawnAmounts;
-    
-    function SplitIt() payable public {
+      address[] employees = [${addresses.join(", ") }];
+      uint totalReceived;
+      mapping (address => uint) withdrawnAmounts;
+
+      function SplitIt() payable public {
         updateTotalReceived();
-    }
-    
-    function () payable public {
+      }
+
+      function () payable public {
         updateTotalReceived();
-    }
-    
-    function updateTotalReceived() internal {
+      }
+
+      function updateTotalReceived() internal {
         totalReceived += msg.value;
-    }
-    
-    modifier canWithdraw() {
-        
+      }
+
+      modifier canWithdraw() {
+
         bool contains = false;
-        
+
         for(uint i = 0; i < employees.length; i++) {
-            if (employees[i] == msg.sender) {
-                contains = true;
-            }
+          if (employees[i] == msg.sender) {
+            contains = true;
+          }
         }
-        
         require(contains);
         _;
-        
-    }
-    
-    function withdraw() canWithdraw public {
-        
+      }
+
+      function withdraw() canWithdraw public {
+
         uint amountAllocated = totalReceived/employees.length;
         uint amountWithdrawn = withdrawnAmounts[msg.sender];
         uint amount = amountAllocated - amountWithdrawn;
         withdrawnAmounts[msg.sender] = amountWithdrawn + amount;
-        
-        if (amount > 0) {
-           msg.sender.transfer(amount);
-        }
-        
-    }
-    
-}`
 
-document.getElementById("codePreviewParagraph").innerHTML = contractCode
-	
+        if (amount > 0) {
+          msg.sender.transfer(amount);
+        }
+      }
+    }`
+
+  document.getElementById("codePreviewParagraph").innerHTML = contractCode
 }
 
 function publishContract() {
-	
 	web3.eth.compile.solidity(contractCode, (error, compiled) => {
-			
 		console.log(error || compiled[":SplitIt"])
 
-		web3.eth.sendTransaction({data:compiled[":SplitIt"].bytecode}, (error, transactionHash) => { 
-			
+    web3.eth.sendTransaction({data:compiled[":SplitIt"].bytecode}, (error, transactionHash) => {
 			console.log(error || transactionHash)
-			
+
 			if (!error) {
-
 				var statusDisplay = document.getElementById("transactionObjectParagraph")
-				statusDisplay.innerHTML = "Pending" 
+				statusDisplay.innerHTML = "Pending"
+
 				var checkInterval = window.setInterval(() => {
-					
 					web3.eth.getTransaction(transactionHash, (error, transaction) => {
-						
 						console.log(error || transaction)
+
 						statusDisplay.innerHTML = statusDisplay.innerHTML + "."
-						
+
 						if (!error && transaction.blockNumber) {
-							
+
 							web3.eth.getTransactionReceipt(transactionHash, (error, receipt) => {
-								
 								console.log(error || receipt)
-		
-								statusDisplay.innerHTML = receipt.contractAddress 
+								statusDisplay.innerHTML = receipt.contractAddress
 								clearInterval(checkInterval)
-						
 							})
-							
 						} else if (error) {
-							
 							clearInterval(checkInterval)
-							
 						}
-						
-						
 					})
-					
-					
-	
-					
 				}, 5000)
-
 			}
-			
 		})
-	
 	})
-
 }
